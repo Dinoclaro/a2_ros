0490103ef37bd2527b03268c954a76df4adf88f0
David Jensen <dhjensen02@gmail.com>
2026-06-23 15:31:56 +0200

    updated node support files

--- src/control/a2_orchestrator/setup.py
+++ src/control/a2_orchestrator/setup.py
@@ -1,16 +1,30 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'a2_orchestrator'
+
+data_files = [
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+]
+
+for path in glob('launch/*.launch.py'):
+    data_files.append(
+        (os.path.join('share', package_name, 'launch'), [path])
+    )
+
+for path in glob('config/*.yaml'):
+    data_files.append(
+        (os.path.join('share', package_name, 'config'), [path])
+    )
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Dino',
@@ -21,6 +35,8 @@ setup(
     entry_points={
         'console_scripts': [
             'velocity_publisher = a2_orchestrator.velocity_publisher:main',
+            'mission_orchestrator = a2_orchestrator.mission_orchestrator:main',
+            'detection_logger = a2_orchestrator.detection_logger:main',
         ],
     },
 )
